8ae11fa347df3a6877b6f56d589f199b49360301
Ghifari Nur <thinkpadgipey@gmail.com>
2026-04-01 14:19:49 +0700

    update deploy.sh
    update deploy.sh

--- k8s/production/deploy.sh
+++ k8s/production/deploy.sh
@@ -7,7 +7,7 @@ set -e
 
 # Configuration
 NAMESPACE="sporteventbook"
-HARBOR_URL="harbor.your-domain.com"
+HARBOR_URL="registry.bercalab.my.id"
 HARBOR_USERNAME="admin"
 IMAGE_TAG="v1.0.0"
 DOMAIN="sport.bercalab.my.id"
